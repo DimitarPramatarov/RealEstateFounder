2277338ad22b7353cb712787d676e6d43787b4ac
Dimitar Pramatarov <51478619+DimitarPramatarov@users.noreply.github.com>
2021-06-19 15:45:44 +0300

    Initial commit

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstateFounder.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    
+    [ApiController]
+    [Route("controller")]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RealEstateFounder
+ This is a web api wich looking for real estates wich a published for sell by owner
